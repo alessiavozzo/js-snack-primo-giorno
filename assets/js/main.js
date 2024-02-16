e5c7eb740791202118b0b8095c83282c6b14727e
alessiavozzo <a.vozzo95@gmail.com>
2024-02-16 17:59:06 +0100

    add extra ex execution

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,5 +35,30 @@ Successivamente, modifica il valore della variabile in "Marco".
 Stampa il nuovo valore in console. */
 
 let theName = "Carlo"; //string
-theName = "Marco"
+theName = "Marco"; //string
 console.log(theName);
+
+/* Esercizio 4
+Dichiara una variabile chiamata temperature e assegnale il valore 22.5. Successivamente, aumenta il suo valore di 5 e stampa il nuovo valore in console. */
+
+let temperature = 22.5; //number
+temperature = 22.5 + 5;
+console.log(temperature);
+
+/* Esercizio 5: Numeri
+Rispondete in un commento a più righe: Che differenza c'è tra il numero 3.14 e 100 sono diversi? Perchè? Che tipo di dato è uno e che tipo di dato è l'altro? */
+
+/* 100 è un numero intero, 3.14 è un numero decimale arrotondato e corrisponde al valore del π. Esiste una proprietà in JS dell'oggetto Math, Math.PI, che restituisce in automatico il valore del pi greco.*/
+
+console.log(Math.PI);
+
+/* Esercizio 6: console
+Per scrivere dentro la nostra console utilizziamo la sua funzione .log() ma se scrivessimo console.info('Il mio messaggio') che differenza c'è? Perche?
+Esistono altri metodi da usare con console. ? */
+
+console.info("il mio messaggio");
+
+/* visivamente non cambia nulla rispetto al console.log (tranne in firefox dove appare una i di fianco in caso di console.info). Console.log viene usato comunemente per debugging, mentre console.info viene utilizzato per messaggi informativi. Esistono anche messaggi con la console di tipo .warn o .error) */
+
+console.warn("non funziona niente");
+console.error("sono un errore");
